d7084854158e44ba84b1c812090ac6d80f142c57
Diya Sundeep <diyasundeep64@gmail.com>
2024-05-17 00:06:55 +0530

    deleted start

--- src/pages/server.tsx
+++ src/pages/server.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
+import app from "@/app/firebase";
 import "tailwindcss/tailwind.css";
 const Server = () => {
-  return <div>server</div>;
+  return (
+    <div className="h-screen bg-black text-white flex justify-center ">
+      server
+      <button className="p-2 bg-gray-800 hover:bg-gray-700 rounded-md">
+        Start
+      </button>
+    </div>
+  );
 };
 
 export default Server;
