62b984648d58889b7c41336ba73697e44a621eb6
Diya Sundeep <diyasundeep64@gmail.com>
2024-05-17 00:11:53 +0530

    server updating

--- src/pages/server.tsx
+++ src/pages/server.tsx
@@ -11,11 +11,21 @@ import app from "@/app/firebase";
 import "tailwindcss/tailwind.css";
 const Server = () => {
   return (
-    <div className="h-screen bg-black text-white flex justify-center ">
-      server
-      <button className="p-2 bg-gray-800 hover:bg-gray-700 rounded-md">
-        Start
-      </button>
+    <div className="h-screen bg-black text-white flex text-center flex-col space-y-5">
+      <div> server</div>
+      <div>
+        <button className="p-2 bg-gray-800 hover:bg-gray-700 rounded-md w-1/6">
+          Start
+        </button>
+      </div>
+      <div className="flex flex-row justify-center">
+        <div className="flex flex-col">
+          <div> User : </div>
+          <div> Car : </div>
+          <div> Charge :</div>
+          <div className="font-bold font-lg">79%</div>
+        </div>
+      </div>
     </div>
   );
 };
